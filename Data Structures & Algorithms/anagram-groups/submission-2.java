c141c1a94c888f0150125468c50ff8502eafd5e1
Shivam Wadhwa <105790527+wadhwashivam@users.noreply.github.com>
2026-08-09 10:15:42 -0400

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> hm = new HashMap<>();
+
+        for (String s : strs){
+            char[] c = s.toCharArray();
+            Arrays.sort(c);
+            String key = new String(c);
+
+            hm.putIfAbsent(key, new ArrayList<>());
+            hm.get(key).add(s);
+        }
+
+        return new ArrayList<>(hm.values());
+    }
+}
