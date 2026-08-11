d2985fb17bf181dada72f6ac6c6a641e4be9595f
Shivam Wadhwa <105790527+wadhwashivam@users.noreply.github.com>
2026-08-11 11:35:57 -0400

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashMap<Integer, Set<Character>> rows = new HashMap<>();
+        HashMap<Integer, Set<Character>> cols = new HashMap<>();
+        HashMap<String, Set<Character>> squares = new HashMap<>();
+
+        for (int r = 0;r<9;r++){
+            for (int c = 0;c<9;c++){
+                if (board[r][c] == '.'){
+                    continue;
+                }
+
+                String squareKey = (r/3) + "," + (c/3);
+
+                if (rows.computeIfAbsent(r, k -> new HashSet<>()).contains(board[r][c]) || cols.computeIfAbsent(c, k -> new HashSet<>()).contains(board[r][c]) || squares.computeIfAbsent(squareKey, k -> new HashSet<>()).contains(board[r][c])){
+                    return false;
+                }
+                rows.get(r).add(board[r][c]);
+                cols.get(c).add(board[r][c]);
+                squares.get(squareKey).add(board[r][c]);
+            }
+        }
+        return true;
+    }
+}
