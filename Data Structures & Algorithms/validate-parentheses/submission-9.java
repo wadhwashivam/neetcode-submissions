f814c1887c5756b8c00573ceb930109c868c9c3b
Shivam Wadhwa <105790527+wadhwashivam@users.noreply.github.com>
2026-09-07 11:04:20 -0400

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()){
+            if(c == '[' || c == '{' || c == '('){
+                stack.push(c);
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top = stack.pop();
+                if(top != '{' && c == '}'){
+                    return false;
+                }
+                if(top != '(' && c == ')'){
+                    return false;
+                }
+                if (top!= '[' && c == ']'){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
